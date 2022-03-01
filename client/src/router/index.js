24e66520d08cf2632c2f96c428463915aaf794e5
Antony Quintero <antonyquinterog@gmail.com>
2022-03-01 00:37:22 -0600

    Adding functionality on Front-End Application

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import AddPost from "../views/AddPost.vue";
+import Post from "../views/Post.vue";
+import EditPost from "../views/EditPost.vue";
 
 Vue.use(VueRouter);
 
@@ -24,6 +26,16 @@ const routes = [
     path: "/add-post",
     name: "add-post",
     component: AddPost,
+  },
+  {
+    path: "/post/:id",
+    name: "post",
+    component: Post,
+  },
+  {
+    path: "/post/:id",
+    name: "edit-post",
+    component: EditPost,
   },
 ];
 
